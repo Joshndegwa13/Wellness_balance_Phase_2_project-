0f0ad2cd808222181aabcf0a64502b7a63c3af50
Joshndegwa13 <ndegwajosh7@gmail.com>
2024-08-19 00:14:03 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {BrowserRouter as Router,Routes,Route,Navigate,} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
@@ -12,12 +12,12 @@ import LogIn from "./components/Login";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import "./styles/App.css";
 
-const auth = getAuth();   //initializing firebase auth 
+const auth = getAuth(); // initializing firebase auth 
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);  //state to track user log in status(logged in or not)   
-  
-  //useEffect to monitor authentication state changes 
+  const [isAuthenticated, setIsAuthenticated] = useState(false); // state to track user log in status (logged in or not)   
+
+  // useEffect to monitor authentication state changes 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setIsAuthenticated(!!user);
@@ -28,32 +28,36 @@ function App() {
   return (
     <ThemeProvider>
       <Router>
-        <Navbar />
-        <Routes>
-          <Route
-            path="/"
-            element={isAuthenticated ? <Home /> : <Navigate to="/login" />}
-          />
-          <Route
-            path="/workout-plan"
-            element={
-              isAuthenticated ? <WorkoutPlan /> : <Navigate to="/login" />
-            }
-          />
-          <Route
-            path="/progress-tracker"
-            element={
-              isAuthenticated ? <ProgressTracker /> : <Navigate to="/login" />
-            }
-          />
-          <Route
-            path="/settings"
-            element={isAuthenticated ? <Settings /> : <Navigate to="/login" />}
-          />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<LogIn />} />
-        </Routes>
-        <Footer />
+        <div className="wrapper"> {/* Added wrapper class */}
+          <Navbar />
+          <main>
+            <Routes>
+              <Route
+                path="/"
+                element={isAuthenticated ? <Home /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/workout-plan"
+                element={
+                  isAuthenticated ? <WorkoutPlan /> : <Navigate to="/login" />
+                }
+              />
+              <Route
+                path="/progress-tracker"
+                element={
+                  isAuthenticated ? <ProgressTracker /> : <Navigate to="/login" />
+                }
+              />
+              <Route
+                path="/settings"
+                element={isAuthenticated ? <Settings /> : <Navigate to="/login" />}
+              />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/login" element={<LogIn />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
       </Router>
     </ThemeProvider>
   );
